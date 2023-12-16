e5cb1f208f0851eb9ed9d533f6b287402f9d1a99
Chris Tran <christophert@noreply>
2023-12-16 17:30:14 -0500

    update use subLDAP query to account for nested LDAP groups; linting

--- sync.py
+++ sync.py
@@ -9,24 +9,24 @@ from pathlib import Path
 load_dotenv()
 logging.basicConfig(level=logging.INFO)
 
-if not os.environ.get('PVE_DEST_REALM'):
+if not os.environ.get("PVE_DEST_REALM"):
     print("Destination realm required")
     sys.exit(1)
 
-dest_realm = os.environ.get('PVE_DEST_REALM')
+dest_realm = os.environ.get("PVE_DEST_REALM")
 
 # get CA bundle path
-if (os.environ.get('CA_BUNDLE')):
-    ca_bundle_path = Path(os.environ.get('CA_BUNDLE')).resolve()
+if os.environ.get("CA_BUNDLE"):
+    ca_bundle_path = Path(os.environ.get("CA_BUNDLE")).resolve()
 
 # do ldap query
 try:
     if ca_bundle_path:
         ldap.set_option(ldap.OPT_X_TLS_CACERTFILE, str(ca_bundle_path))
-    elif os.environ.get('TLS_VERIFY', True):
+    elif os.environ.get("TLS_VERIFY", True):
         ldap.set_option(ldap.OPT_X_TLS_REQUIRE_CERT, ldap.OPT_X_TLS_NEVER)
-    ldapobj = ldap.initialize(os.environ.get('LDAP_URI'))
-    ldapobj.simple_bind_s(os.environ.get('LDAP_USER'), os.environ.get('LDAP_PASS'))
+    ldapobj = ldap.initialize(os.environ.get("LDAP_URI"))
+    ldapobj.simple_bind_s(os.environ.get("LDAP_USER"), os.environ.get("LDAP_PASS"))
 except ldap.INVALID_CREDENTIALS:
     print("Invalid credentials")
     sys.exit(1)
@@ -35,19 +35,30 @@ except ldap.LDAPError as e:
     sys.exit(1)
 
 # get LDAP membership
-results = ldapobj.search_s(os.environ.get('LDAP_BASE_DN'),
-                          ldap.SCOPE_SUBTREE,
-                          os.environ.get('LDAP_SEARCH_FILTER'),
-                          ['member'])
+results = ldapobj.search_s(
+    os.environ.get("LDAP_BASE_DN"),
+    ldap.SCOPE_SUBTREE,
+    os.environ.get("LDAP_SEARCH_FILTER"),
+    ["dn"],
+)
 
 proxmox = proxmoxer.ProxmoxAPI(
-    os.environ.get('PVE_HOST'),
-    user=os.environ.get('PVE_USER'),
-    password=os.environ.get('PVE_PASS'),
-    verify_ssl=ca_bundle_path if ca_bundle_path else False)
+    os.environ.get("PVE_HOST"),
+    user=os.environ.get("PVE_USER"),
+    password=os.environ.get("PVE_PASS"),
+    verify_ssl=ca_bundle_path if ca_bundle_path else False,
+)
 
-proxmox_openid_users = [user['userid'] for user in proxmox.access.users.get() if user['realm-type'] == "openid"]
-proxmox_openid_groups = [group['groupid'] for group in proxmox.access.groups.get() if dest_realm in group['groupid']]
+proxmox_openid_users = [
+    user["userid"]
+    for user in proxmox.access.users.get()
+    if user["realm-type"] == "openid"
+]
+proxmox_openid_groups = [
+    group["groupid"]
+    for group in proxmox.access.groups.get()
+    if dest_realm in group["groupid"]
+]
 
 user_group_assoc = {}
 groups_to_create = []
@@ -55,23 +66,34 @@ groups_to_delete = []
 
 # parse through LDAP membership and assemble list for Proxmox
 for result in results:
-    group, params = result
-    groupid = f"{group.split(',')[0].split('=')[1]}-{dest_realm}"
+    group_dn, params = result
+    groupid = f"{group_dn.split(',')[0].split('=')[1]}-{dest_realm}"
     if groupid not in proxmox_openid_groups:
         groups_to_create.append(groupid)
-    if 'member' in params:
-        # users exist in this group
-        uids = [f"{user.split('=')[1]}@{dest_realm}" for user in (str(dn).split(',')[0] for dn in params['member'])]
-        for uid in uids:
-            if uid not in user_group_assoc:
-                user_group_assoc[uid] = [groupid]
-            else:
-                user_group_assoc[uid].append(groupid)
+
+    # use subLDAP query in order to pull nested members since Proxmox doesn't support nested group membership
+    # this query only works on non-LDAP systems that support nested group membership. AD requires a different LDAP query
+    user_member_query_result = ldapobj.search_s(
+        os.environ.get("LDAP_BASE_DN"),
+        ldap.SCOPE_SUBTREE,
+        f"(&(objectClass=person)(memberOf={group_dn}))",
+        ["dn"],
+    )
+
+    user_members = [user_dn[0] for user_dn in user_member_query_result]
+    for user_dn in user_members:
+        user = f"{user_dn.split(',')[0].split('=')[1]}@{dest_realm}"
+        if user not in user_group_assoc:
+            user_group_assoc[user] = [groupid]
+        else:
+            user_group_assoc[user].append(groupid)
 
 # create groups
 for group in groups_to_create:
     logging.info("Creating group: %s", group)
-    proxmox.access.groups.post(groupid=group, comment="LDAP/OpenID Sync Managed DO NOT EDIT")
+    proxmox.access.groups.post(
+        groupid=group, comment="LDAP/OpenID Sync Managed DO NOT EDIT"
+    )
 
 # map users
 for user, groups in user_group_assoc.items():
